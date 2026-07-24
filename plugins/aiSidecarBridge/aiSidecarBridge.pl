524f1df3e1ed8a15720672b278639291306ace60
Iskandar Sulaili <admin@cakobox.com>
2026-07-24 11:59:28 +0800

    fix: portal lock blocks ALL commands for 15s (not just duplicate moves)

--- plugins/aiSidecarBridge/aiSidecarBridge.pl
+++ plugins/aiSidecarBridge/aiSidecarBridge.pl
@@ -2934,15 +2934,10 @@ sub _rewrite_runtime_command {
 		$_cur_map =~ s/\.gat$//;
 		# Direct portal coordinate - always pass through
 		if ($_target eq '22 203') {
-			# Check if portal walk lock is active - if so, block this duplicate
-			my $_pl_check = $_last_reflex_fire_ms{'portal_walk_lock'} || 0;
-			if ($_pl_check > 0 && _now_ms() < $_pl_check) {
-				debug "[portal_lock] blocking duplicate portal move - walk in progress\n", 'aiSidecarBridge', 2;
-				return ('', 'portal_walk_lock');
-			}
 			debug "[move_rewrite] portal coordinate 22 203 - passing through\n", 'aiSidecarBridge', 2;
-			# Set a 10-second lock to prevent PDCA from interrupting the portal walk
-			$_last_reflex_fire_ms{'portal_walk_lock'} = _now_ms() + 10000;
+			# Set a 15-second lock to prevent PDCA from interrupting the portal walk
+			# This blocks ALL subsequent commands until the bot has time to walk through portal
+			$_last_reflex_fire_ms{'portal_walk_lock'} = _now_ms() + 15000;
 			return ($command, 'coordinate_move_raw');
 		}
 
