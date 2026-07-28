9d7df6af04dcb98f51ce8684669ecc04f0bad46a
Iskandar Sulaili <admin@cakobox.com>
2026-07-29 04:34:57 +0800

    fix: add  to Globals import for keepalive ping

--- plugins/aiSidecarBridge/aiSidecarBridge.pl
+++ plugins/aiSidecarBridge/aiSidecarBridge.pl
@@ -6,7 +6,7 @@ use feature 'state';
 
 use Commands;
 use FileParsers qw(parseConfigFile);
-use Globals qw(%config $char $field @ai_seq $net %monsters %players %npcs $monstersList $playersList $npcsList %timeout);
+use Globals qw(%config $char $field @ai_seq $net %monsters %players %npcs $monstersList $playersList $npcsList %timeout $messageSender);
 use IO::Socket::INET;
 use Globals qw($char);
 use Log qw(debug message warning);
